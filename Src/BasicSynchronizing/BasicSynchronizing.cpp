08cf787e2414099896695facf766097dd30c48d3
FaberSanZ <faber.reach@gmail.com>
2025-07-31 16:09:35 -0500

    Add BasicSync sample

--- Src/BasicSynchronizing/BasicSynchronizing.cpp
+++ Src/BasicSynchronizing/BasicSynchronizing.cpp
@@ -1,14 +1,14 @@
-// DepthTests.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// BasicSynchronizing.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <Windows.h>
+//#include <Windows.h>
 #include <d3d12.h>
 #include <dxgi1_4.h>
 #include <tchar.h>
 #include <iostream>
 #include <d3dcompiler.h>
 #include "ShaderCompiler.h"
-
+#include "Windows.h"
 #pragma comment(lib, "d3d12.lib")
 #pragma comment(lib, "dxgi.lib")
 
@@ -131,8 +131,20 @@ public:
     ID3D12RootSignature* rootSignature = nullptr;
     Core::ShaderCompilerDXC shaderCompiler {};
 
-	DescriptorHeap rtvDescriptorHeap {};  // This is a heap for our render target view descriptor
+    // Set the viewport and scissor rect
+    D3D12_VIEWPORT viewport = { };
+    D3D12_RECT scissorRect = { };
+
+    DescriptorHeap rtvDescriptorHeap {};  // This is a heap for our render target view descriptor
     DescriptorHeap dpvDescriptorHeap {};  // This is a heap for our depth/stencil buffer descriptor
+
+
+
+    // Synchronization objects.
+    UINT m_frameIndex;
+    HANDLE m_fenceEvent;
+    ID3D12Fence* m_fence;
+    UINT64 m_fenceValue;
 
 
 
@@ -140,6 +152,10 @@ public:
     {
         m_Width = width;
         m_Height = Heigh;
+
+
+        viewport = { 0, 0, (float)m_Width, (float)m_Height, 0.0f, 1.0f };
+        scissorRect = { 0, 0, (long)m_Width, (long)m_Height };
 
 
         IDXGIFactory4* factory = nullptr;
@@ -175,20 +191,6 @@ public:
 
 
 
-        // Create RTV descriptor heap
-        rtvDescriptorHeap.Initialize(device, D3D12_DESCRIPTOR_HEAP_TYPE_RTV, m_FrameCount);
-
-        for (uint32_t i = 0; i < m_FrameCount; ++i)
-        {
-            ID3D12Resource* backBuffer = nullptr;
-            swapChain->GetBuffer(i, IID_PPV_ARGS(&backBuffer));
-
-
-            D3D12_CPU_DESCRIPTOR_HANDLE rtvHandle = rtvDescriptorHeap.GetCPUHandle(i);
-            device->CreateRenderTargetView(backBuffer, nullptr, rtvHandle);
-
-            renderTargets[i] = backBuffer;
-        }
 
 
         device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&commandAlloc));
@@ -196,16 +198,75 @@ public:
 
         commandList->Close();
 
-
+        CreateSynchronizationObjects();
+        CreateRenderTargetViews();
         CreateDepthBuffer();
         CreatePipeline();
         CreateVertexBuffer();
         CreateIndexBuffer();
         return true;
     }
+
+    void CreateSynchronizationObjects()
+    {
+        m_frameIndex = swapChain->GetCurrentBackBufferIndex();
+        m_fenceValue = 0;
+        // Create a fence for synchronization
+        device->CreateFence(m_fenceValue, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&m_fence));
+        // Create an event handle to use for frame synchronization
+        m_fenceEvent = CreateEvent(nullptr, false, false, nullptr);
+
+        if (m_fenceEvent == nullptr)
+        {
+            throw std::runtime_error("Failed to create fence event.");
+        }
+
+        WaitForPreviousFrame();
+	}
+
+    void WaitForPreviousFrame()
+    {
+        // WAITING FOR THE FRAME TO COMPLETE BEFORE CONTINUING IS NOT BEST PRACTICE.
+        // This is code implemented as such for simplicity. The D3D12HelloFrameBuffering
+        // sample illustrates how to use fences for efficient resource usage and to
+        // maximize GPU utilization.
+
+        // Signal and increment the fence value.
+        const UINT64 fence = m_fenceValue;
+        commandQueue->Signal(m_fence, fence);
+        m_fenceValue++;
+
+        // Wait until the previous frame is finished.
+        if (m_fence->GetCompletedValue() < fence)
+        {
+            m_fence->SetEventOnCompletion(fence, m_fenceEvent);
+            WaitForSingleObject(m_fenceEvent, INFINITE);
+        }
+
+        m_frameIndex = swapChain->GetCurrentBackBufferIndex();
+    }
+
+
+    void CreateRenderTargetViews()
+    {
+        // Create RTV descriptor heap
+        rtvDescriptorHeap.Initialize(device, D3D12_DESCRIPTOR_HEAP_TYPE_RTV, m_FrameCount);
+
+        for (uint32_t i = 0; i < m_FrameCount; ++i)
+        {
+            ID3D12Resource* backBuffer = nullptr;
+            swapChain->GetBuffer(i, IID_PPV_ARGS(&backBuffer));
+            D3D12_CPU_DESCRIPTOR_HANDLE rtvHandle = rtvDescriptorHeap.GetCPUHandle(i);
+            device->CreateRenderTargetView(backBuffer, nullptr, rtvHandle);
+            renderTargets[i] = backBuffer;
+		}
+
+    }
+
+
     void CreateDepthBuffer()
     {
-		// Create depth stencil buffer
+        // Create depth stencil buffer
         D3D12_RESOURCE_DESC depthStencilDesc = {};
         depthStencilDesc.Dimension = D3D12_RESOURCE_DIMENSION_TEXTURE2D;
         depthStencilDesc.Width = m_Width;
@@ -218,14 +279,14 @@ public:
         depthStencilDesc.Layout = D3D12_TEXTURE_LAYOUT_UNKNOWN;
         depthStencilDesc.Flags = D3D12_RESOURCE_FLAG_ALLOW_DEPTH_STENCIL;
 
-		// Clear value for the depth stencil buffer
+        // Clear value for the depth stencil buffer
         D3D12_CLEAR_VALUE clearValue = {};
         clearValue.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
         clearValue.DepthStencil.Depth = 1.0f;
         clearValue.DepthStencil.Stencil = 0;
 
 
-		// Create heap properties for the depth stencil buffer
+        // Create heap properties for the depth stencil buffer
         D3D12_HEAP_PROPERTIES heapProps = {};
         heapProps.Type = D3D12_HEAP_TYPE_DEFAULT;
         heapProps.CPUPageProperty = D3D12_CPU_PAGE_PROPERTY_UNKNOWN;
@@ -234,14 +295,14 @@ public:
         heapProps.VisibleNodeMask = 1;
 
 
-		// Create the depth stencil buffer resource
+        // Create the depth stencil buffer resource
         device->CreateCommittedResource(&heapProps, D3D12_HEAP_FLAG_NONE, &depthStencilDesc, D3D12_RESOURCE_STATE_DEPTH_WRITE, &clearValue, IID_PPV_ARGS(&depthStencilBuffer));
 
-		// Create descriptor heap for depth stencil view (DSV)
-		dpvDescriptorHeap.Initialize(device, D3D12_DESCRIPTOR_HEAP_TYPE_DSV, 1);
+        // Create descriptor heap for depth stencil view (DSV)
+        dpvDescriptorHeap.Initialize(device, D3D12_DESCRIPTOR_HEAP_TYPE_DSV, 1);
 
 
-		// Create the depth stencil view (DSV)
+        // Create the depth stencil view (DSV)
         D3D12_DEPTH_STENCIL_VIEW_DESC dsvDesc = {};
         dsvDesc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
         dsvDesc.ViewDimension = D3D12_DSV_DIMENSION_TEXTURE2D;
@@ -253,8 +314,8 @@ public:
 
     void CreatePipeline()
     {
-        auto vertexShaderBlob = shaderCompiler.Compile(L"../../../../Assets/Shaders/DepthTests/VertexShader.hlsl", L"VS", L"vs_6_0");
-        auto pixelShaderBlob = shaderCompiler.Compile(L"../../../../Assets/Shaders/DepthTests/PixelShader.hlsl", L"PS", L"ps_6_0");
+        auto vertexShaderBlob = shaderCompiler.Compile(L"../../../../Assets/Shaders/BasicSync/VertexShader.hlsl", L"VS", L"vs_6_0");
+        auto pixelShaderBlob = shaderCompiler.Compile(L"../../../../Assets/Shaders/BasicSync/PixelShader.hlsl", L"PS", L"ps_6_0");
 
 
         D3D12_ROOT_SIGNATURE_DESC rootSigDesc = {};
@@ -304,7 +365,7 @@ public:
         psoDesc.BlendState = blendDesc;
 
         // Depth stencil
-  
+
 
 
         D3D12_DEPTH_STENCIL_DESC depthStencilDesc = {};
@@ -339,18 +400,17 @@ public:
 
         Vertex vertices[] =
         {
-            // Red Quad
-            { -0.5f,  0.5f, 0.2f, 1.0f,   1.0f, 0.0f, 0.0f, 1.0f },
-            {  0.5f, -0.5f, 0.2f, 1.0f,   1.0f, 0.0f, 0.0f, 1.0f },
-            { -0.5f, -0.5f, 0.2f, 1.0f,   1.0f, 0.0f, 0.0f, 1.0f },
-            {  0.5f,  0.5f, 0.2f, 1.0f,   1.0f, 0.0f, 0.0f, 1.0f },
+            { -0.20f, -0.55f, 0.0f, 1.0f,   0.8f, 0.2f, 0.0f, 1.0f },
+            { -0.55f,  0.55f, 0.0f, 1.0f,   0.8f, 0.2f, 0.0f, 1.0f },
+            {  0.15f,  0.55f, 0.0f, 1.0f,   0.8f, 0.2f, 0.0f, 1.0f },
 
-            // Yellow Quad
-            { -0.75f,  0.75f, 0.3f, 1.0f,   1.0f, 1.0f, 0.0f, 1.0f },
-            {  0.0f,   0.0f,  0.3f, 1.0f,   1.0f, 1.0f, 0.0f, 1.0f },
-            { -0.75f,  0.0f,  0.3f, 1.0f,   1.0f, 1.0f, 0.0f, 1.0f },
-            {  0.0f,   0.75f, 0.3f, 1.0f,   1.0f, 1.0f, 0.0f, 1.0f },
+            {  0.00f, -0.40f, 0.0f, 1.0f,   0.3f, 1.0f, 0.3f, 1.0f },
+            { -0.35f,  0.70f, 0.0f, 1.0f,   0.3f, 1.0f, 0.3f, 1.0f },
+            {  0.35f,  0.70f, 0.0f, 1.0f,   0.3f, 1.0f, 0.3f, 1.0f },
 
+            {  0.20f, -0.55f, 0.0f, 1.0f,   0.3f, 0.3f, 1.0f, 1.0f },
+            { -0.15f,  0.55f, 0.0f, 1.0f,   0.3f, 0.3f, 1.0f, 1.0f },
+            {  0.55f,  0.55f, 0.0f, 1.0f,   0.3f, 0.3f, 1.0f, 1.0f },
         };
 
         vertexBuffer.size = sizeof(vertices);
@@ -396,19 +456,15 @@ public:
     void CreateIndexBuffer()
     {
         // Define indices for a triangle
-        uint32_t indices[] = 
+        uint32_t indices[] =
         {
-            // GREEN quad
-            0, 1, 2,
-            0, 3, 1,
-
-            // BLUE quad
-            4, 5, 6,
-            4, 7, 5,
+            0, 1, 2, 
+            3, 4, 5, 
+            6, 7, 8 
         };
         indexBuffer.size = sizeof(indices);
         indexBuffer.stride = sizeof(uint32_t);
-		indexBuffer.m_indexCount = _countof(indices);
+        indexBuffer.m_indexCount = _countof(indices);
 
         // Create index buffer
         D3D12_HEAP_PROPERTIES heapProps = {};
@@ -444,7 +500,7 @@ public:
         indexBuffer.m_indexBufferView.SizeInBytes = indexBuffer.size;
     }
 
-    void Loop()
+    void OnRender()
     {
         // get the current back buffer index
         uint32_t backBufferIndex = swapChain->GetCurrentBackBufferIndex();
@@ -458,13 +514,13 @@ public:
         // get a handle to the depth/stencil buffer
         D3D12_CPU_DESCRIPTOR_HANDLE dsvHandle = dpvDescriptorHeap.GetCPUHandle(0);
 
-		// get a handle to the render target view (RTV) for the current back buffer
+        // get a handle to the render target view (RTV) for the current back buffer
         D3D12_CPU_DESCRIPTOR_HANDLE rtvHandle = rtvDescriptorHeap.GetCPUHandle(backBufferIndex);
 
 
         // set the render target for the output merger stage (the output of the pipeline)
         // Set the render target view (RTV) for the current back buffer
-		// Set the depth/stencil view (DSV) for the current frame
+        // Set the depth/stencil view (DSV) for the current frame
         commandList->OMSetRenderTargets(1, &rtvHandle, false, &dsvHandle);
 
         commandList->ClearDepthStencilView(dsvHandle, D3D12_CLEAR_FLAG_DEPTH, 1.0f, 0, 0, nullptr);
@@ -475,11 +531,9 @@ public:
         float clearColor[] = { 0.0f, 0.2f, 0.4f, 1.0f };
         commandList->ClearRenderTargetView(rtvHandle, clearColor, 0, nullptr);
 
-        // Set the viewport and scissor rect
-        D3D12_VIEWPORT view = { 0, 0, m_Width, m_Height, 0.0f, 1.0f };
-        D3D12_RECT scissorRect = { 0, 0, m_Width, m_Height };
 
-        commandList->RSSetViewports(1, &view);
+
+        commandList->RSSetViewports(1, &viewport);
         commandList->RSSetScissorRects(1, &scissorRect);
 
         // draw the triangle
@@ -498,6 +552,44 @@ public:
 
         // Present the frame
         swapChain->Present(1, 0);
+
+		// Signal and increment the fence value.
+		// This will be used to synchronize the GPU and CPU.
+        WaitForPreviousFrame();
+    }
+
+
+
+
+    void OnResize(uint32_t newWidth, uint32_t newHeight)
+    {
+        m_Width = newWidth;
+        m_Height = newHeight;
+
+        // Esperar GPU si estás usando varios buffers
+        commandQueue->Signal(m_fence, ++m_fenceValue);
+        WaitForPreviousFrame();
+
+        // Liberar buffers antiguos
+        for (int i = 0; i < m_FrameCount; ++i)
+        {
+            if (renderTargets[i]) renderTargets[i]->Release();
+        }
+        if (depthStencilBuffer) depthStencilBuffer->Release();
+
+        // Resize swap chain
+        swapChain->ResizeBuffers(m_FrameCount, m_Width, m_Height, DXGI_FORMAT_R8G8B8A8_UNORM, 0);
+
+        // Volver a crear RTVs
+        CreateRenderTargetViews();
+
+        // Volver a crear el Depth Buffer
+        CreateDepthBuffer();
+
+        // Viewport & Scissor Rect
+        viewport = { 0.0f, 0.0f, static_cast<float>(m_Width), static_cast<float>(m_Height), 0.0f, 1.0f };
+        scissorRect = { 0, 0, static_cast<long>(m_Width), static_cast<long>(m_Height) };
+
     }
 
 
@@ -509,19 +601,19 @@ public:
         if (vertexBuffer.m_vertexBuffer)
             vertexBuffer.Destroy();
 
-		if (depthStencilBuffer)
-			depthStencilBuffer->Release();
+        if (depthStencilBuffer)
+            depthStencilBuffer->Release();
 
 
         if (pipelineState)
-			pipelineState->Release();
-            
+            pipelineState->Release();
+
         for (uint32_t i = 0; i < 2; ++i)
             if (renderTargets[i])
                 renderTargets[i]->Release();
 
         rtvDescriptorHeap.Destroy();
-		dpvDescriptorHeap.Destroy();
+        dpvDescriptorHeap.Destroy();
 
         if (swapChain)
             swapChain->Release();
@@ -538,48 +630,50 @@ public:
         if (commandList)
             commandList->Release();
     }
-
-
 };
+
+
+
+
 
 int main()
 {
-    uint32_t width = 1280;
-    uint32_t height = 820;
-    const wchar_t title[] = L"DX12 DepthTests";
+
+    WindowApp win = { 1280u, 720u, L"DX12 Basic Sync" };
+	win.Initialize(GetModuleHandle(nullptr));
+
+
+
+    std::cout << win.GetWidth() << std::endl;
+    std::cout << win.GetHeight() << std::endl;
+
+
 
     Render render {};
+	render.Initialize(win.GetHWND(), win.GetWidth(), win.GetHeight());
 
 
-    WNDCLASSEX wcex = { sizeof(WNDCLASSEX), CS_CLASSDC, DefWindowProc, 0L, 0L, GetModuleHandle(nullptr), nullptr, nullptr, nullptr, nullptr, title, nullptr };
-    RegisterClassEx(&wcex);
-
-    // Create a window
-    HWND hWnd = CreateWindow(title, title, WS_OVERLAPPEDWINDOW, 100, 100, width, height, nullptr, nullptr, wcex.hInstance, nullptr);
-
-    ShowWindow(hWnd, SW_SHOW);
-
-    if (!render.Initialize(hWnd, width, height))
-    {
-        std::cerr << "Error DirectX 12" << std::endl;
-        return 1;
-    }
-
-    MSG msg = { 0 };
-    while (msg.message != WM_QUIT)
-    {
-        if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+    win.SetOnUpdate([&render]
         {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
-        else
-        {
-            render.Loop();
-        }
-    }
 
-    render.Cleanup();
+        });
+
+
+    win.SetOnRender([&render]
+        {
+            //std::cout << "Render called" << std::endl;
+            render.OnRender();
+        });
+
+
+    win.SetOnResize([&render](UINT w, UINT h)
+        {
+            std::wcout << L"[RESIZE] -> " << w << L"x" << h << std::endl;
+            render.OnResize(w, h);
+        });
+
+
+    win.Run();
 
     return 0;
 }
